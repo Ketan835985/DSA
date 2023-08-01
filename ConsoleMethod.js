e82bb2f71ac8819c0b999015d671f8729f6a4e52
Ketan Gupta <guptaketan6375@gmail.com>
2023-08-01 08:43:31 +0530

    string paranthesis

---
+++ ConsoleMethod.js
@@ -0,0 +1,33 @@
+const company  = {
+    name: 'Google',
+    city: 'Mountain View',
+    state: 'CA'
+}
+
+// console.table(company)
+
+/*
+┌─────────┬─────────────────┐
+│ (index) │     Values      │
+├─────────┼─────────────────┤
+│  name   │    'Google'     │
+│  city   │ 'Mountain View' │
+│  state  │      'CA'       │
+└─────────┴─────────────────┘
+ */
+// console.debug(company)
+
+/*
+{ name: 'Google', city: 'Mountain View', state: 'CA' }
+*/
+
+
+// console.assert(company.name === 'Google')
+
+// console.count(company)
+/*
+[object Object]: 1
+*/
+
+
+// console.table(console)
